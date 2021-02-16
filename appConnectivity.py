966428063e7a654ebdb68dfcbd16fc65456fc80f
tienvinhle <tienvinh.industrial.automation@gmail.com>
2021-02-16 22:10:19 +0700

    edit space

--- appConnectivity.py
+++ appConnectivity.py
@@ -5,7 +5,7 @@ import functools
 mb1 = None
 
 def deviceInit(eventloop):
-    global mb1
+	global mb1
 	mb1 = ModbusDevice('192.168.0.106', 8080, eventloop)
 	print('devices have been created successfully')
 
@@ -16,8 +16,8 @@ async def updateValue(period):
 	global mb1
 	while True:
 		mb1.read_holding_Reg()
-        callRedisClient()
-        await asyncio.sleep(1)
+		callRedisClient()
+		await asyncio.sleep(1)
 
 async def main():
 	loop = asyncio.get_running_loop()
